5a07736c42677a0f10b6a8843e479e629be3c3f5
kookmoom <kookmoom@gmail.com>
2013-11-23 14:56:13 +0800

    update

---
+++ LiteGALTests/LiteGALTests.m
@@ -0,0 +1,34 @@
+//
+//  LiteGALTests.m
+//  LiteGALTests
+//
+//  Created by Artuira on 13-11-21.
+//  Copyright (c) 2013年 Artuira. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LiteGALTests : XCTestCase
+
+@end
+
+@implementation LiteGALTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
